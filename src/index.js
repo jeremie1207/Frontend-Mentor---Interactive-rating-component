8bae2d28e55e4f7a9bfcb063998757a1a7a1af75
Jeremie Barhabazi <71102646+jeremie1207@users.noreply.github.com>
2026-01-06 15:24:35 -0500

    Refactor form and rank handling in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
-import {form, ranks} from './constants/constant'
-import { formHandler, rankHandler } from './utils/utils';
+const form = document.querySelector(".rank_form");
+const ranks = document.querySelectorAll("input[name='rank']");
+const rankValue= document.querySelector('.rank_value');
+const thanksState = document.querySelector('.thanks');
+
+let rateValue;
+
+const removeDataState = () => {
+  ranks.forEach(rank => {
+    document.querySelector(`label[for="rank-${rank.value}"]`).removeAttribute("data-state");
+  })
+}
+
+
+const setActive =  id => {
+  removeDataState();
+  document.querySelector(`label[for="rank-${id}"]`).setAttribute("data-state", "active");
+}
+
+const formHandler = e => {
+  e.preventDefault();
+
+  if(!rateValue) {
+    alert('Please select a rating before submitting!');
+    return;
+  };
+
+  thanksState.removeAttribute("hidden");
+  form.classList.add("display-none");
+}
+
+const rankHandler = rank => {
+  rank.addEventListener('change', () => {
+    rateValue = rank.value;
+    setActive(rateValue);
+    rankValue.textContent = rateValue;
+  });
+} 
 
 
 ranks.forEach(rankHandler);
 form.addEventListener("submit", formHandler);
+
 
