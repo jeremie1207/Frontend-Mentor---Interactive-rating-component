8d7f581f3202a8f08ebfeacb846b984ccb5651d7
jeremie1207 <jbarh086@uottawa.ca>
2026-01-07 03:27:05 -0500

    refactoring

--- src/constants/constant.js
+++
@@ -1,4 +0,0 @@
-export const form = document.querySelector(".rank_form");
-export const ranks = document.querySelectorAll("input[name='rank']");
-export const rankValue= document.querySelector('.rank_value');
-export const thanksState = document.querySelector('.thanks');
--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
 const form = document.querySelector(".rank_form");
 const ranks = document.querySelectorAll("input[name='rank']");
-const rankValue= document.querySelector('.rank_value');
-const thanksState = document.querySelector('.thanks');
+const rankValue = document.querySelector(".rank_value");
+const thanksState = document.querySelector(".thanks");
 
 let rateValue;
 
 const removeDataState = () => {
-  ranks.forEach(rank => {
-    document.querySelector(`label[for="rank-${rank.value}"]`).removeAttribute("data-state");
-  })
-}
+  ranks.forEach((rank) => {
+    document
+      .querySelector(`label[for="rank-${rank.value}"]`)
+      .removeAttribute("data-state");
+  });
+};
 
-
-const setActive =  id => {
+const setActive = (id) => {
   removeDataState();
-  document.querySelector(`label[for="rank-${id}"]`).setAttribute("data-state", "active");
-}
+  document
+    .querySelector(`label[for="rank-${id}"]`)
+    .setAttribute("data-state", "active");
+};
 
-const formHandler = e => {
+const formHandler = (e) => {
   e.preventDefault();
 
-  if(!rateValue) {
-    alert('Please select a rating before submitting!');
+  if (!rateValue) {
+    alert("Please select a rating before submitting!");
     return;
-  };
+  }
 
   thanksState.removeAttribute("hidden");
   form.classList.add("display-none");
-}
+};
 
-const rankHandler = rank => {
-  rank.addEventListener('change', () => {
+const rankHandler = (rank) => {
+  rank.addEventListener("change", () => {
     rateValue = rank.value;
     setActive(rateValue);
     rankValue.textContent = rateValue;
   });
-} 
-
+};
 
 ranks.forEach(rankHandler);
 form.addEventListener("submit", formHandler);
-
-
